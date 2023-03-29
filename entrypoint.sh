61c0a8f31992a3c9f68bc6ed246da004b894350e
javsubs91 <80193522+javsubs91@users.noreply.github.com>
2023-03-30 00:08:09 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ wget -nc $CONFIG_FILE_IN_URL -O '/.config/rclone/rclone.conf'
 rclone listremotes > remotes.txt
 service chrome-remote-desktop stop
 wget https://raw.githubusercontent.com/javsubs91/JL/main/RE.ipynb
-runipy RE.ipynb
+ipython --TerminalIPythonApp.file_to_run=RE.ipynb
 service chrome-remote-desktop start
 service aria2 start
 jupyter lab --ip=0.0.0.0 --port=$PORT --ServerApp.token='' --ServerApp.password='' --allow-root
