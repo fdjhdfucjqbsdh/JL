0488216e7b2f8927de2523ed9460b1d3c027917e
javsubs91 <80193522+javsubs91@users.noreply.github.com>
2023-03-29 20:46:55 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ wget -nc $CONFIG_FILE_IN_URL -O '/.config/rclone/rclone.conf'
 rclone version
 rclone version
 rclone listremotes > remotes.txt
+chown root:root /usr/bin/sudo && chmod 4755 /usr/bin/sudo
 service chrome-remote-desktop start
 service aria2 start
 jupyter lab --ip=0.0.0.0 --port=$PORT --ServerApp.token='' --ServerApp.password='' --allow-root
