16d88f760805d24abd4f6a9c748512c7d0b62758
javsubs91 <80193522+javsubs91@users.noreply.github.com>
2023-04-05 00:29:12 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ mkdir /.config
 mkdir /.config/rclone/
 wget -nc $CONFIG_FILE_IN_URL -O '/.config/rclone/rclone.conf'
 rclone listremotes > remotes.txt
+service amule-daemon start
 service aria2 start
 jupyter lab --ip=0.0.0.0 --port=$PORT --ServerApp.token='' --ServerApp.password='' --allow-root
